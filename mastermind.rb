8ff8f1505dc11bf6a098e12e4f161825a5bebf56
Lance <dogmandevices@gmail.com>
2024-04-07 14:38:13 -0500

    create player, human, and computer classes with roles

--- mastermind.rb
+++ mastermind.rb
@@ -2,15 +2,30 @@ module Mastermind
   MAX_TURNS = 12
 
   class Game
-    attr_accessor :colors
-    def initialize
+    attr_reader :colors
+    def initialize(player1, player2)
       @colors = ["red", "green", "blue", "yellow", "purple", "orange"]
-      #@players = [codemaker, codebreaker]
+      @players = [player1.new(self, :codemaker), player2.new(self, :codebreaker)]
     end
 
     def get_random_colors
       selection = @colors.shuffle.slice(0,4)
     end
+  end
+
+  class Player
+    def initialize(game, role)
+      @game = game
+      @role = role
+    end
+  end
+
+  class Human < Player
+
+  end
+
+  class Computer < Player
+
   end
 
 end
