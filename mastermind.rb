f9b1afa77c6390bc365a84b1bce6d0d6fc2ec735
Lance <dogmandevices@gmail.com>
2024-04-07 15:24:48 -0500

    create guess method, game loop, and lose, psuedocode guess checking

--- mastermind.rb
+++ mastermind.rb
@@ -2,19 +2,60 @@ module Mastermind
   MAX_TURNS = 12
 
   class Game
-    attr_reader :colors, :players, :role
+    attr_reader :colors, :players, :role, :remaining_turns
     def initialize(player1, player2)
       @colors = ["red", "green", "blue", "yellow", "purple", "orange"]
       @players = [player1.new(self, :codemaker), player2.new(self, :codebreaker)]
       puts "The computer has a code of four colors. You must guess both the colors and the order."
       puts "You have twelve turns to break the computer's code!"
       puts "The possible colors are: #{@colors.join(", ").}"
+      puts "The colors will not repeat, and each place in the code has a color."
+      @remaining_turns = 12
     end
 
     def get_random_colors
       @selection = @colors.shuffle.slice(0,4)
     end
-    attr_reader :selection
+    attr_accessor :selection
+
+    def play
+      while @remaining_turns <= MAX_TURNS
+        get_random_colors
+        guess = @players[1].get_guess
+        if correct?(guess)
+          puts "You won! You had #{:remaining_turns} left."
+          break
+        elsif lose?
+          puts "You lose! Better luck next time."
+          puts "The answer was #{@selection.join(", ")}."
+          break
+        else
+          puts "Keep trying!"
+          return
+        end
+      end
+    end
+
+    def correct?(g)
+      # for each color in selection
+      # compare with guess
+      # unless completely correct:
+        # first check if any colors are correct, regardless of position
+        # then check if any positions are correct
+        # print which colors were correct
+        # print which positions were also correct
+        # @remaining_Turns -= 1
+        # return false
+      # if completely correct
+        # return true
+    end
+    def lose?
+      if @remaining_turns <= 0
+        true
+      else
+        false
+      end
+    end
   end
 
   class Player
@@ -28,6 +69,23 @@ module Mastermind
     def to_s
       "Human Player, #{@role}"
     end
+    attr_accessor :guesses
+    def get_guess
+      guess = []
+      puts "What is your guess for the first color?"
+      one = gets.chomp
+      guess.push(one)
+      puts "And the second?"
+      two = gets.chomp
+      guess.push(two)
+      puts "Third?"
+      three - gets.chomp
+      guess.push(three)
+      puts "Final position?"
+      four = gets.chomp
+      guess.push(four)
+      puts "Your guess is #{@guess.join(", ")}."
+      guess
   end
 
   class Computer < Player  
