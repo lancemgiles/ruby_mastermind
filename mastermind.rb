922f5ccbdd31f1f7676ea8614270751c0524901e
Lance <dogmandevices@gmail.com>
2024-04-07 14:58:36 -0500

    game initialize explains the rules; player classes roles are properly dfined

--- mastermind.rb
+++ mastermind.rb
@@ -2,15 +2,19 @@ module Mastermind
   MAX_TURNS = 12
 
   class Game
-    attr_reader :colors
+    attr_reader :colors, :players, :role
     def initialize(player1, player2)
       @colors = ["red", "green", "blue", "yellow", "purple", "orange"]
       @players = [player1.new(self, :codemaker), player2.new(self, :codebreaker)]
+      puts "The computer has a code of four colors. You must guess both the colors and the order."
+      puts "You have twelve turns to break the computer's code!"
+      puts "The possible colors are: #{@colors.join(", ").}"
     end
 
     def get_random_colors
-      selection = @colors.shuffle.slice(0,4)
+      @selection = @colors.shuffle.slice(0,4)
     end
+    attr_reader :selection
   end
 
   class Player
@@ -21,15 +25,17 @@ module Mastermind
   end
 
   class Human < Player
-
+    def to_s
+      "Human Player, #{@role}"
+    end
   end
 
-  class Computer < Player
-
+  class Computer < Player  
+    def to_s
+      "Computer Player, #{@role}"
+    end
   end
-
 end
 
 include Mastermind
-game = Game.new()
-puts game.get_random_colors
+game = Game.new(Computer, Human)
